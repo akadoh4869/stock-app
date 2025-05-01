162fef7d1366c8031f47ac1098ff0530f1fb8524
akadoh4869 <42401982+akadoh4869@users.noreply.github.com>
2025-05-01 22:47:38 +0900

    管理者画面修正

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,10 +8,17 @@ use App\Models\User; // ← これを忘れずに！
 class AdminController extends Controller
 {
     //
+    // public function index()
+    // {
+    //     $userCount = User::count(); // ユーザー数を取得
+    //     // return view('admin.index', compact('userCount')); // ビューに渡す
+    //     return view('admin.dashboard', compact('userCount')); 
+    // }
     public function index()
     {
-        $userCount = User::count(); // ユーザー数を取得
-        // return view('admin.index', compact('userCount')); // ビューに渡す
-        return view('admin.dashboard', compact('userCount')); 
+        $users = \App\Models\User::select('user_name')->get();
+        $userCount = $users->count();
+
+        return view('admin.dashboard', compact('users', 'userCount'));
     }
 }
